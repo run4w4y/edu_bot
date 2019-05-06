5267cdc9ffa503154dd3a7b4ac3d32ee7d783231
run4w4y <add4che@gmail.com>
2019-05-06 04:40:08 +0300

    ProxyHandler integration

--- bot.py
+++ bot.py
@@ -7,11 +7,13 @@ from bot_config import token, adminlist, proxy
 from edu_parser.profile import Profile
 from edu_parser.exceptions import *
 from datetime import datetime
+from proxy_handler import ProxyHandler
 import os
 
 bot = telegram.Bot(token=token)
 updater = Updater(token=token)
 dispatcher = updater.dispatcher
+proxies = ProxyHandler(proxy_path='good_proxies.txt')
 
 users = {}
 
@@ -32,8 +34,24 @@ def check_creds(f):
                 del users[chat]
             except KeyError:
                 pass
-            bot.send_message(chat_id=chat, text="Ваш логин или пароль более не подходит для входа в систему, для того, чтобы продолжить работу с ботом используйте команду /start и введите ваши новые данные для входа")
+            bot.send_message(chat_id=chat,
+                             text="Ваш логин или пароль более не подходит для входа в систему, для того, "
+                                  "чтобы продолжить работу с ботом используйте команду /start и введите ваши новые "
+                                  "данные для входа")
             return ConversationHandler.END
+
+    return wrap
+
+
+def use_proxy(f):
+    def wrap(bot, update):
+        global users
+        chat = update.message.chat_id
+        users[chat].change_proxy(new_proxy=proxies.get_proxy(chat_id=chat))
+        res = f(bot, update)
+        users.chat.logout()
+        proxies.free_proxy(chat_id=chat)
+        return res
 
     return wrap
 
@@ -49,7 +67,7 @@ def start(bot, update):
     bot.send_message(chat_id=chat, text="Здравствуйте! Для того, чтобы начать работу с ботом вам необходимо "
                                         "предоставить ваш логин и пароль от личного кабинета edu.tatar.ru в формате "
                                         "логин:пароль")
-    
+
     return START_CREDENTIALS
 
 
@@ -65,10 +83,10 @@ def credentials(bot, update):
     except BaseException:
         bot.send_message(chat_id=chat, text="Неправильный формат данных. Попробуйте еще раз.")
         return START_CREDENTIALS
-    
+
     try:
         bot.send_message(chat_id=chat, text="Пожалуйста, подождите...")
-        users[chat] = Profile(creds, proxy=proxy)
+        users[chat] = Profile(creds, proxy=proxies.get_proxy(chat))
     except CredentialsError:
         bot.send_message(chat_id=chat, text="Неправильный логин или пароль. Попробуйте еще раз.")
         return START_CREDENTIALS
@@ -76,6 +94,9 @@ def credentials(bot, update):
     bot.send_message(chat_id=chat, text="Авторизация прошла успешно. Начните работу с ботом с команды /help")
     with open('credentials/{}.txt'.format(chat), 'w') as f:
         f.write(str(creds))
+
+    users[chat].logout()
+    proxies.free_proxy(chat)
     return ConversationHandler.END
 
 
@@ -90,15 +111,16 @@ def helpp(bot, update):
                 '/diary_day - показать страницу дневника за указанный день; \n' \
                 '/profile_info - показать данные профиля; \n' \
                 '/check_grades - проверить наличие новых оценок; \n' \
-                '/predict - предсказать средний балл за указанный предмет (имя предмета не чувствительно к регистру, также не обязательно писать его полностью, достаточно, например "русский" или "обж"); \n' \
+                '/predict - предсказать средний балл за указанный предмет (имя предмета не чувствительно к регистру, ' \
+                'также не обязательно писать его полностью, достаточно, например "русский" или "обж"); \n' \
                 '/cancel - отменить действие.'
-    
+
     bot.send_message(chat_id=chat, text=reply_msg, parse_mode=telegram.ParseMode.HTML)
 
 
 def cancel(bot, update):
     chat = update.message.chat_id
-    
+
     bot.send_message(chat_id=chat, text='Команда отменена', reply_markup=ReplyKeyboardRemove())
     return ConversationHandler.END
 
@@ -109,7 +131,7 @@ def get_profile_info(bot, update):
     chat = update.message.chat_id
 
     user_data = users[chat].data
-    reply_msg = '<b>Информация о вашем профиле:</b>\n'
+    reply_msg = '<b>Информация о вашем профиле:</b>\n\n'
     translation = {
         'name': 'Имя',
         'login': 'Логин',
@@ -121,18 +143,19 @@ def get_profile_info(bot, update):
     }
     for key, value in user_data.items():
         reply_msg += '<b>{}</b>: {}\n'.format(translation[key], value)
-    
+
     bot.send_message(chat_id=chat, text=reply_msg, parse_mode=telegram.ParseMode.HTML)
 
 
 @check_creds
+@use_proxy
 def get_diary_curterm(bot, update):
     global users
     chat = update.message.chat_id
 
     bot.send_message(chat_id=chat, text="Пожалуйста, подождите...")
-    diary = users[chat].diary_term(draw=True, draw_path=str(chat)+'grades.png')
-    bot.send_photo(chat_id=chat, photo=open(str(chat)+'grades.png', 'rb'))
+    diary = users[chat].diary_term(draw=True, draw_path=str(chat) + 'grades.png')
+    bot.send_photo(chat_id=chat, photo=open(str(chat) + 'grades.png', 'rb'))
 
 
 def get_diary_term(bot, update):
@@ -143,6 +166,7 @@ def get_diary_term(bot, update):
 
 
 @check_creds
+@use_proxy
 def get_diary_numterm(bot, update):
     global users
     chat = update.message.chat_id
@@ -150,16 +174,18 @@ def get_diary_numterm(bot, update):
     try:
         reply = int(update.message.text)
     except ValueError:
-        bot.send_message(chat_id=chat, text="Неправильный формат. Проверьте правильность введенных данных и попробуйте еще раз")
+        bot.send_message(chat_id=chat,
+                         text="Неправильный формат. Проверьте правильность введенных данных и попробуйте еще раз")
         return GET_TERM
 
     bot.send_message(chat_id=chat, text="Пожалуйста, подождите...")
-    diary = users[chat].diary_term(term=str(reply), draw=True, draw_path=str(chat)+'grades' + str(reply) + '.png')
-    bot.send_photo(chat_id=chat, photo=open(str(chat)+'grades' + str(reply) + '.png', 'rb'))
+    diary = users[chat].diary_term(term=str(reply), draw=True, draw_path=str(chat) + 'grades' + str(reply) + '.png')
+    bot.send_photo(chat_id=chat, photo=open(str(chat) + 'grades' + str(reply) + '.png', 'rb'))
     return ConversationHandler.END
 
 
 @check_creds
+@use_proxy
 def get_diary_today(bot, update):
     global users
     chat = update.message.chat_id
@@ -177,10 +203,10 @@ def get_diary_today(bot, update):
         6: 'Воскресенье'
     }
     reply_msg = '<b>Страница дневника на {} - {}:\n</b>'.format(weekday[diary.weekday], diary.date_str)
-    
+
     for subject in diary.subjects:
         new_line = '<b>{} - {}:</b>\nДомашнаяя работа: {}\nКомментарий: {}\nОценки:\n'.format(
-            subject.time, subject.name, subject.homework, subject.comment    
+            subject.time, subject.name, subject.homework, subject.comment
         )
         for grade in subject.grades:
             new_line += '{} - {}\n'.format(grade.grade, grade.comment)
@@ -198,6 +224,7 @@ def get_diary_day(bot, update):
 
 
 @check_creds
+@use_proxy
 def get_diary_numday(bot, update):
     global users
     chat = update.message.chat_id
@@ -206,7 +233,8 @@ def get_diary_numday(bot, update):
     try:
         datetime.strptime(reply, '%d.%m.%Y')
     except BaseException:
-        bot.send_message(chat_id=chat, text="Неправильный формат. Проверьте правильность введенных данных и попробуйте еще раз")
+        bot.send_message(chat_id=chat,
+                         text="Неправильный формат. Проверьте правильность введенных данных и попробуйте еще раз")
         return GET_DAY
 
     bot.send_message(chat_id=chat, text="Пожалуйста, подождите...")
@@ -222,10 +250,10 @@ def get_diary_numday(bot, update):
         6: 'Воскресенье'
     }
     reply_msg = '<b>Страница дневника на {} - {}:\n</b>'.format(weekday[diary.weekday], diary.date_str)
-    
+
     for subject in diary.subjects:
         new_line = '<b>{} - {}:</b>\nДомашнаяя работа: {}\nКомментарий: {}\nОценки:\n'.format(
-            subject.time, subject.name, subject.homework, subject.comment    
+            subject.time, subject.name, subject.homework, subject.comment
         )
         for grade in subject.grades:
             new_line += '{} - {}\n'.format(grade.grade, grade.comment)
@@ -238,31 +266,37 @@ def get_diary_numday(bot, update):
 
 def predict(bot, update):
     chat = update.message.chat_id
-    bot.send_message(chat_id=chat, text='Введите предмет и оценки, которые вы планируете получить в формате "Английский 5 5 5 5"')
-    
+    bot.send_message(chat_id=chat,
+                     text='Введите предмет и оценки, которые вы планируете получить в формате "Английский 5 5 5 5"')
+
     return PREDICT_SUBJECT
 
 
 @check_creds
+@use_proxy
 def predict_subject(bot, update):
     global users
 
     chat = update.message.chat_id
     reply = update.message.text.split()
     if len(reply) < 2:
-        bot.send_message(chat_id=chat, text="Неправильный формат. Проверьте правильность введенных данных и попробуйте еще раз (учитывайте, что вы не можете указать менее одной оценки)")
+        bot.send_message(chat_id=chat,
+                         text="Неправильный формат. Проверьте правильность введенных данных и попробуйте еще раз (учитывайте, что вы не можете указать менее одной оценки)")
         return PREDICT_SUBJECT
     diary = users[chat].diary_term()
     subject = diary.get_subject(reply[0])
     if subject is None:
-        bot.send_message(chat_id=chat, text="Неправильный формат. Проверьте правильность введенных данных и попробуйте еще раз (учитывайте, что вы не можете указать менее одной оценки)")
+        bot.send_message(chat_id=chat,
+                         text="Неправильный формат. Проверьте правильность введенных данных и попробуйте еще раз (учитывайте, что вы не можете указать менее одной оценки)")
         return PREDICT_SUBJECT
     new_grades = list(map(int, reply[1:]))
-    bot.send_message(chat_id=chat, text='Ваш новый балл средний балл будет равен {}'.format(subject.predict(new_grades)))
+    bot.send_message(chat_id=chat,
+                     text='Ваш новый балл средний балл будет равен {}'.format(subject.predict(new_grades)))
     return ConversationHandler.END
 
 
 @check_creds
+@use_proxy
 def check_grades(bot, update):
     global users
     chat = update.message.chat_id
@@ -285,7 +319,7 @@ def shutdown(bot, update):
 
     chat = update.message.chat_id
     user = update.message.from_user
-    
+
     if user['username'] not in adminlist:
         bot.send_message(chat_id=chat, text='Извините, по-видимому, у вас нет прав на исполнение данной команды')
     else:
@@ -301,7 +335,7 @@ def main():
 
     for filename in os.listdir('credentials'):
         with open('credentials/' + filename) as f:
-                users[int(filename.split('.txt')[0])] = Profile(ast.literal_eval(f.readline()), proxy=proxy)
+            users[int(filename.split('.txt')[0])] = Profile(ast.literal_eval(f.readline()), proxy=proxy)
     print(users)
 
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
@@ -330,7 +364,7 @@ def main():
 
     get_diary_today_handler = CommandHandler('diary_today', get_diary_today)
     dispatcher.add_handler(get_diary_today_handler)
-    
+
     shutdown_handler = CommandHandler('shutdown', shutdown)
     dispatcher.add_handler(shutdown_handler)
 
--- proxy_handler.py
+++ proxy_handler.py
@@ -2,20 +2,18 @@ import requests
 import ast
 from queue import Queue
 import time
+import threading
 
 
 class ProxyHandler:
-    def check(self, proxy, f):
+    def check(self, proxy):
         try:
             self.session.get('https://edu.tatar.ru/logon', proxies=proxy, timeout=2)
             self.good_proxies.put(proxy)
-
-            f.write(str(proxy) + '\n')
             return True
         except BaseException:
             self.bad_proxies.append(proxy)
             return False
-
 
     def check_bad(self, verbose=True):
         temp = self.bad_proxies
@@ -24,17 +22,14 @@ class ProxyHandler:
             self.check(proxy)
             time.sleep(0.1)
 
-
     def check_all(self, verbose=True):
-        with open('good_proxies.txt', 'w')  as f:
-            self.bad_proxies = []
-            count = 0
-            for proxy in self.all_proxies:
-                count += 1
-                status = self.check(proxy, f)
-                if verbose:
-                    print('[O] -' if status else '[X] -', proxy['https'])
-
+        self.bad_proxies = []
+        count = 0
+        for proxy in self.all_proxies:
+            count += 1
+            status = self.check(proxy)
+            if verbose:
+                print('[O] -' if status else '[X] -', proxy['https'])
 
     def __init__(self, proxy_path='proxies.txt'):
         self.good_proxies = Queue()
@@ -43,8 +38,7 @@ class ProxyHandler:
         self.session = requests.session()
         with open(proxy_path, 'r') as f:
             self.all_proxies = [ast.literal_eval(line) for line in f.readlines()]
-        self.check_all()
-
+        self.run_check()
 
     def get_proxy(self, chat_id):
         while self.good_proxies.empty():
@@ -53,8 +47,12 @@ class ProxyHandler:
         self.busy_proxies[chat_id] = proxy
         return proxy
 
-    
     def free_proxy(self, chat_id):
         proxy = self.busy_proxies[chat_id]
         self.bad_proxies.append(proxy)
         del self.busy_proxies[chat_id]
+
+    def run_check(self):
+        check_thread = threading.Thread(target=self.check_all)
+        check_thread.start()
+        check_thread.join()
